558bc5949691645dde9179e270a3577ac4526c13
Sridhar G <srid@outlook.com>
2013-04-10 19:26:15 +0200

    [MAJOR] By default load the product search screen. Also let spring serve the static resources

--- quadcart-web/src/main/webapp/statics/js/quadcart.js
+++ quadcart-web/src/main/webapp/statics/js/quadcart.js
@@ -1,23 +1,21 @@
 $(document).ready(function() {
-	$("#searchbox").keypress(function(event){
-	    if(event.which ==13 ){
+	$("#searchbox").keypress(function(event) {
+		if (event.which == 13) {
 			var searchText = $(this).val();
 			var queryString = 'q=' + searchText;
-			if (searchText !== '') 
-			{
+			if (searchText !== '') {
 				$.ajax({
 					type : "GET",
-					url : "searchproduct.html",
+					url : "sales/searchproduct.html",
 					data : queryString,
 					cache : false,
-					success : function(html)
-					{
+					success : function(html) {
 						$("#products").html(html).show();
 					}
 				});
 			}
 			return false;
-	    } 
+		}
 		return true;
 	});
 

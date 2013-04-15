f5cf735683c25dee34c6ba54836289302d2f5d99
srid99 <srid99@gmail.com>
2013-04-15 19:37:22 +0200

    [MINOR] Refined Producs object to handle product search

--- quadcart-web/src/main/webapp/statics/js/quadcart.js
+++ quadcart-web/src/main/webapp/statics/js/quadcart.js
@@ -1,30 +1,39 @@
-$(document).ready(function() {
-    $("#searchbox").keyup(function(event) {
-    	Products.handleSearchEvent(event);
-    });
-});
-
 var Products = (function() {
+	var searchBox, productsContainer;
 
-    return {
-    	handleSearchEvent: function(event) {
-            clearTimeout($.data(this, 'timer'));
-            if (event.keyCode == 13) {
-                Products.searchProducts(true);
-            } else {
-                $(this).data('timer', setTimeout(Products.searchProducts, 500));
-            }
-    	},
-        searchProducts: function(force) {
-            var searchText = $("#searchbox").val();
-            if (searchText !== '') {
-                var queryString = '?q=' + searchText;
-                $.get("sales/searchproduct" + queryString, function(products) {
-                    $("#products").html(products).show();
-                });
-            } else {
-                $("#products").empty();
-            }
-        }
-    };
+	handleSearchEvent = function(event) {
+		clearTimeout($.data(this, 'timer'));
+		if (event.keyCode == 13) {
+			searchProducts(true);
+		} else {
+			$(this).data('timer', setTimeout(searchProducts, 500));
+		}
+	}, searchProducts = function(force) {
+		var searchText = searchBox.val();
+		if (searchText !== '') {
+			var queryString = '?q=' + searchText;
+			$.get("sales/searchproduct" + queryString, function(products) {
+				productsContainer.html(products).show();
+			});
+		} else {
+			productsContainer.empty();
+		}
+	};
+
+	return {
+		init : function() {
+			searchBox = $("#searchbox");
+			productsContainer = $("#products");
+		},
+		bindEvents : function() {
+			searchBox.keyup(function(event) {
+				handleSearchEvent(event);
+			});
+		}
+	};
 })();
+
+$(document).ready(function() {
+	Products.init();
+	Products.bindEvents();
+});

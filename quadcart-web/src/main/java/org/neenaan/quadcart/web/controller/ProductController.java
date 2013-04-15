3cf0d331f11665413bd0f5b203f162e4d640e58c
srid99 <srid99@gmail.com>
2013-04-15 18:19:25 +0200

    [TRIVIAL] Restructured the way to return the model map

--- quadcart-web/src/main/java/org/neenaan/quadcart/web/controller/ProductController.java
+++ quadcart-web/src/main/java/org/neenaan/quadcart/web/controller/ProductController.java
@@ -1,13 +1,14 @@
 package org.neenaan.quadcart.web.controller;
 
 import java.util.List;
+
 import org.neenaan.quadcart.bussiness.ProductBO;
 import org.neenaan.quadcart.data.domain.Product;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,12 +26,12 @@ public class ProductController {
     }
 
     @RequestMapping( value = "/**/searchproduct", method = RequestMethod.GET )
-    public void searchProducts( final Model model , @RequestParam( "q" ) final String searchQuery ) {
+    public ModelMap searchProductsFor( @RequestParam( "q" ) final String searchQuery ) {
         LOG.trace( "Search products for given query string [ {} ]", searchQuery );
 
         final List<Product> matchedProducts = productBO.searchProducts( searchQuery );
-        model.addAttribute( "searchResult", matchedProducts );
-
         LOG.debug( "Total of {} products found for the given query", matchedProducts.size() );
+
+        return new ModelMap().addAttribute( "searchResult", matchedProducts );
     }
 }
